bf8c075f6861d2703c3091ea2f71412046a9517c
Santiago Pappier <spappier@gmail.com>
2016-06-22 01:37:27 -0300

    unnecesary underscores + resource has model

--- server/__init__.py
+++ server/__init__.py
@@ -7,24 +7,24 @@ from sqlalchemy_wrapper import SQLAlchemy
 class Server(object):
 
     def __init__(self):
-        self._app = Flask('')
-        self._resources = []
+        self.app = Flask('')
+        self.resources = []
 
     def init_db(self, connection_uri):
-        self._db = SQLAlchemy(connection_uri)
-        return self._db
+        self.db = SQLAlchemy(connection_uri)
+        return self.db
 
     def register(self, model):
         resource = Resource(model)
-        self._resources.append(resource)
-        self._add_rules(resource)
+        self.resources.append(resource)
+        self.add_rules(resource)
         return model
 
     def run_server(self):
-        self._app.run(debug=True)
+        self.app.run(debug=True)
 
-    def _add_rules(self, resource):
-        app = self._app
+    def add_rules(self, resource):
+        app = self.app
         app.add_url_rule('{}/_schema'.format(resource.route),
                          '{}-schema'.format(resource.name),
                          view_func=resource.schema_view())
@@ -39,6 +39,7 @@ class Server(object):
 class Resource(object):
 
     def __init__(self, model):
+        self.model = model
         self.name = model.__tablename__
         self.route = '/{}'.format(self.name)
         self.columns = model.__table__.columns
