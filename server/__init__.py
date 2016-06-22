c4291c80da576130522d38a305fcaae6301e6b08
Santiago Pappier <spappier@gmail.com>
2016-06-22 01:34:58 -0300

    rest urls

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,6 @@
 import json
 
-from flask import Flask
-from flask.views import MethodView
+from flask import Flask, request
 from sqlalchemy_wrapper import SQLAlchemy
 
 
@@ -25,12 +24,19 @@ class Server(object):
         self._app.run(debug=True)
 
     def _add_rules(self, resource):
-        self._app.add_url_rule('{}/_schema'.format(resource.route),
-                               '{}-schema'.format(resource.name),
-                               view_func=resource.schema_view())
+        app = self._app
+        app.add_url_rule('{}/_schema'.format(resource.route),
+                         '{}-schema'.format(resource.name),
+                         view_func=resource.schema_view())
+
+        resource_view = resource.resource_view()
+        app.add_url_rule('{}/'.format(resource.route),
+                         view_func=resource_view, methods=('GET', 'POST'))
+        app.add_url_rule('{}/<int:id>'.format(resource.route),
+                         view_func=resource_view, methods=('GET', 'PUT', 'DELETE'))
 
 
-class Resource(MethodView):
+class Resource(object):
 
     def __init__(self, model):
         self.name = model.__tablename__
@@ -41,3 +47,37 @@ class Resource(MethodView):
         schema = json.dumps(dict(keys=self.columns.keys()))
         return lambda: schema
 
+    def resource_view(self):
+        def view(id=None):
+            resource = view.resource
+            method = request.method
+
+            if method == 'GET':
+                if not id:
+                    return resource.index()
+                else:
+                    return resource.show(id)
+            elif method == 'POST':
+                return resource.create()
+            elif method == 'PUT':
+                return resource.update(id)
+            elif method == 'DELETE':
+                return resource.delete(id)
+
+        view.resource = self
+        return view
+
+    def index(self):
+        return 'index'
+
+    def create(self):
+        return 'create'
+
+    def show(self, id):
+        return 'show'
+
+    def update(self, id):
+        return 'update'
+
+    def delete(self, id):
+        return 'delete'
